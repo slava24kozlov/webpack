9986df87406c7cb678064bf4f812fd747fe4fdcc
Vyacheslav <slava24kozlov@gmail.com>
2024-01-17 00:29:15 +0300

    feat: added typescript to project

--- webpack.common.js
+++ webpack.common.js
@@ -4,89 +4,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
-
-  // Customize the webpack build process
-  plugins: [
-    // Removes/cleans build folders and unused assets when rebuilding
-    new CleanWebpackPlugin(),
-    // Copies files from target to destination folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'public'),
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    // Generates an HTML file from a template
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/template.html'),
-      filename: "index.html",
-      title: 'Webpack App',
-      favicon: path.resolve(__dirname, './src/public/favicon.png'),
-    })
-  ],
-
-  //How the different types of modules will be treated
-  module: {
-    rules: [
-      // JavaScript: Use Babel to transpile React to JavaScript
-      {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults"
-              }],
-              '@babel/preset-react'
-            ]
-          }
-        }]
-      },
-      // Images: Copy public files to build folder
-      {test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource'},
-      // Fonts and SVGs: Inline files
-      {test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline'},
-      //CSS styles
-      {
-        test: /\.(sass|scss|css)$/,
-        //applying styles to DOM elements
-        use: ['style-loader',
-          {
-            //loading styles
-            loader: 'css-loader',
-            options: {sourceMap: true, importLoaders: 1, modules: false}
-          }
-        ]
-      }
-    ],
-  },
-
-  //Options change how modules are resolved
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    extensions: ['.js', '.jsx', '.json'],
-    descriptionFiles: ['package.json'],
-    mainFiles: ['index'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      assets: path.resolve(__dirname, 'public'),
+    entry: {
+        main: './src/index.tsx',
     },
-  },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+
+    // Customize the webpack build process
+    plugins: [
+        // Removes/cleans build folders and unused assets when rebuilding
+        new CleanWebpackPlugin(),
+        // Copies files from target to destination folder
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'public'),
+                    to: 'assets',
+                    globOptions: {
+                        ignore: ['*.DS_Store'],
+                    },
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+        // Generates an HTML file from a template
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: "index.html",
+            title: 'Webpack App',
+            favicon: path.resolve(__dirname, './src/public/favicon.png'),
+        })
+    ],
+
+    //How the different types of modules will be treated
+    module: {
+        rules: [
+            // Use Loader to transpile TypeScript to JavaScript
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            // Images: Copy public files to build folder
+            {test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource'},
+            // Fonts and SVGs: Inline files
+            {test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline'},
+            //CSS styles
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [
+                    //applying styles to DOM elements
+                    'style-loader',
+                    // loading css styles
+                    'css-loader'
+                ]
+            }
+        ],
+    },
+
+    //Options change how modules are resolved
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        extensions: ['.tsx', '.ts', '.js'],
+        descriptionFiles: ['package.json'],
+        mainFiles: ['index'],
+    },
 }
